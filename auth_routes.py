a57ab00dad204ba81f19f05119dcda120fc7dbd9
FLX <cauerast@gmail.com>
2025-10-28 01:22:21 -0300

    HTTP exceptions and schemas

--- auth_routes.py
+++ auth_routes.py
@@ -1,7 +1,9 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from models import User
 from dependencies import get_session
 from main import bcrypt_context
+from schemas import UserSchema
+from sqlalchemy.orm import Session
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
@@ -16,13 +18,13 @@ async def home():
             }
 
 @auth_router.post("/create_account")
-async def createAccount(name: str, email: str, password: str, session=Depends(get_session)):
-    user = session.query(User).filter(User.email==email).first()
+async def createAccount(user_schema: UserSchema, session: Session = Depends(get_session)):
+    user = session.query(User).filter(User.email==user_schema.email).first()
     if user:
-        return {"message": "There is already a user with this email"}
+        raise HTTPException(status_code=400, detail="There is already a user with this email")
     else:
-        crypt_password = bcrypt_context.hash(password)
-        new_user = User(name, email, crypt_password)
+        crypt_password = bcrypt_context.hash(user_schema.password)
+        new_user = User(user_schema.name, user_schema.email, crypt_password, user_schema.active, user_schema.admin)
         session.add(new_user)
         session.commit()
-        return {"message": "User registered successfully"}
+        return {"message": f"User registered successfully {user_schema.email}"}
